9dd562ff5ce031177f6b2a9cd4a4f3b76c6f7f49
Selin Su Varol <selinsuvarol@gmail.com>
2022-07-21 16:45:55 +0300

    refactor(index.js): removed carousel component from indexjs
    forgot to delete component from index.js, removed it, formatted with npm run format

--- src/components/carousel/__test__/Carousel.test.jsx
+++ src/components/carousel/__test__/Carousel.test.jsx
@@ -1,8 +1,6 @@
 import renderer from "react-test-renderer";
 import Carousel from "../Carousel";
 
-
-
 it("renders correctly", () => {
     const tree = renderer.create(<Carousel />).toJSON();
     expect(tree).toMatchSnapshot();
