621c03c233780570a7cdb25f5ead9ee6476589f9
beyzabostanci <beyza_bostanc@hotmail.com>
2022-07-21 22:25:21 +0300

    feat: added footer component

---
+++ src/components/layout/Footer/__test__/Footer.test.jsx
@@ -0,0 +1,8 @@
+import renderer from "react-test-renderer";
+
+import Footer from "../Footer";
+
+it("renders correctly", () => {
+    const tree = renderer.create(<Footer />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,6 +1,12 @@
 import * as React from "react";
 
+import Footer from "./Footer/Footer";
+
 export default function Layout({ children }) {
     // Put Header or Footer around the children element
-    return <>{children}</>;
+    return (
+        <>
+            {children} <Footer />{" "}
+        </>
+    );
 }
