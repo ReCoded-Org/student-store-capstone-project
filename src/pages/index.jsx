27aa66674bc4a2173c07821e24bf8f11921499e9
NouhRastanawi <noahrastdark@gmail.com>
2022-07-23 18:47:26 +0300

    chore(index): remove my part from index
    removing my part from index before merging
    
    #4

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,12 +3,7 @@ import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import * as React from "react";
 
-import styles from "../styles/headers.module.css";
-
-import Filter from "@/components/filter";
-import catergories from "@/components/filter/categories";
 import Layout from "@/components/layout/Layout";
-import PriceFilter from "@/components/price-filter";
 
 export default function HomePage() {
     const { t } = useTranslation("common");
@@ -26,31 +21,6 @@ export default function HomePage() {
                 <Link href='/' locale='tr'>
                     <a>Turkish</a>
                 </Link>
-            </div>
-            <div className='px-[15px] py-5'>
-                <div className='invisible mx-5 my-2 grid gap-5 text-xl font-bold text-purple lg:visible lg:grid-cols-3'>
-                    <div className='col-span-2'>
-                        <h1 className={styles.brush}>Categories</h1>
-                    </div>
-                    <div>
-                        <h1 className={styles.brush}>Price</h1>
-                    </div>
-                </div>
-                <div className='grid overflow-hidden rounded-full bg-clay xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3'>
-                    <div className='col-span-2 my-3 grid grid-flow-col justify-center overflow-x-auto scrollbar-hide xs:gap-1 md:gap-8'>
-                        {catergories.map((catergory, i) => (
-                            <Filter
-                                key={catergory[i].id}
-                                bgColor={catergory[i].color}
-                                svg={catergory[i].svg}
-                                name={catergory[i].name}
-                            />
-                        ))}
-                    </div>
-                    <div className='my-3 flex items-center justify-center'>
-                        <PriceFilter />
-                    </div>
-                </div>
             </div>
         </Layout>
     );
