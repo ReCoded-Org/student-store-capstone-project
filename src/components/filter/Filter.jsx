df827c0784781d04d74716ad10676b2bcec9c133
NouhRastanawi <noahrastdark@gmail.com>
2022-07-23 16:26:52 +0300

    feat(component): add category label
    Labels for categories added
    
    #4

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,16 +1,24 @@
-const Filter = ({ bgColor, svg }) => {
+const Filter = ({ bgColor, svg, name }) => {
     return (
-        <div
-            className={`flex h-20 w-20 items-center justify-center rounded-full xs:scale-[50%] sm:m-1 sm:scale-90 md:scale-90 lg:scale-100 ${bgColor}`}
-        >
-            <svg viewBox='0 0 24 24' className='h-12 w-12 shrink fill-white'>
-                <path
-                    className='text-center'
-                    fillRule='evenodd'
-                    clipRule='evenodd'
-                    d={`${svg}`}
-                />
-            </svg>
+        <div>
+            <div
+                className={`flex h-20 w-20 items-center justify-center rounded-full xs:scale-[50%] sm:m-1 sm:scale-90 md:scale-90 lg:scale-100 ${bgColor}`}
+            >
+                <svg
+                    viewBox='0 0 24 24'
+                    className='h-12 w-12 shrink fill-white'
+                >
+                    <path
+                        className='text-center'
+                        fillRule='evenodd'
+                        clipRule='evenodd'
+                        d={svg}
+                    />
+                </svg>
+            </div>
+            <h1 className='text-center text-extraDarkPurple xs:mt-[-18px] xs:scale-75 xs:pb-2 sm:mt-0 md:scale-90 md:pb-0'>
+                {name}
+            </h1>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         screens: {
-            xs: "250px",
+            xs: "200px",
             sm: "640px",
             md: "768px",
             lg: "1024px",
