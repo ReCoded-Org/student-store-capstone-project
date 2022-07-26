c72f168adc6c40613ccd89f9e6a50874fa48a585
NouhRastanawi <noahrastdark@gmail.com>
2022-07-26 13:45:32 +0300

    feat(product single page): add product single page
    I started with the product single page. It is not complete yet. Will work more on responsivness and
    the map part.
    
    #17

---
+++ src/pages/products/[id].jsx
@@ -0,0 +1,141 @@
+/* eslint-disable @next/next/no-img-element */
+// Once the data is ready we can use this function to get the paths
+
+// export const getStaticPaths = async () => {
+//     const res = await fetch("");
+//     const products = await res.json();
+
+//     const paths = products((product) => {
+//         return {
+//             params: { id: product.id.toString() },
+//         };
+//     });
+
+//     return {
+//         paths,
+//         fallback: false,
+//     };
+// };
+
+// import styles from "../../styles/headers.module.css";
+
+const Details = () => {
+    return (
+        <div className='bg-clay p-10'>
+            <div className='grid md:grid-cols-2 md:gap-2'>
+                {/* product photos & info */}
+                <div className='grid gap-3 md:grid-rows-2'>
+                    {/* photos */}
+                    <div className='grid items-center justify-center'>
+                        {/* main photo */}
+                        <img
+                            src='https://images.eq3.com/image-service/084d1830-05e2-4319-98d8-88d1e3091fe1/green-velvet-sofa-with-marble-coffee-table_COMPRESSED.png'
+                            alt='Sofa'
+                            className='w-[32rem] rounded-xl'
+                        />
+                    </div>
+                    <div className='grid justify-center xs:scale-90 xs:grid-cols-3 xs:gap-4 md:scale-100 md:grid-cols-3'>
+                        {/* the 3 photos */}
+                        <div className='grid justify-center'>
+                            <img
+                                src='https://images.eq3.com/image-service/084d1830-05e2-4319-98d8-88d1e3091fe1/green-velvet-sofa-with-marble-coffee-table_COMPRESSED.png'
+                                alt='Sofa'
+                                className='w-[9rem] rounded-xl'
+                            />
+                        </div>
+                        <div className='grid justify-center'>
+                            <img
+                                src='https://images.eq3.com/image-service/084d1830-05e2-4319-98d8-88d1e3091fe1/green-velvet-sofa-with-marble-coffee-table_COMPRESSED.png'
+                                alt='Sofa'
+                                className='w-[9rem] rounded-xl'
+                            />
+                        </div>
+                        <div className='grid justify-center'>
+                            <img
+                                src='https://images.eq3.com/image-service/084d1830-05e2-4319-98d8-88d1e3091fe1/green-velvet-sofa-with-marble-coffee-table_COMPRESSED.png'
+                                alt='Sofa'
+                                className='w-[9rem] rounded-xl'
+                            />
+                        </div>
+                    </div>
+                </div>
+                {/* <div className='grid grid-rows-6 xs:scale-90 xs:px-0 md:px-4 lg:scale-100 lg:px-10'> */}
+                <div className='grid grid-flow-row xs:scale-90 xs:px-0 md:px-4 lg:scale-100 lg:px-10'>
+                    {/* Details */}
+                    <div className='grid'>
+                        {/* Headers */}
+                        {/* <div className='col-span-3'> */}
+                        <div>
+                            {/* <div> */}
+                            <h1 className='text-3xl font-bold text-purple'>
+                                Two Seat Sofa
+                            </h1>
+                            <h2 className='text-darkPurple'>
+                                <b>Category:</b> Furniture
+                            </h2>
+                            <h2 className='pb-2 text-darkPurple'>
+                                <b>Condition:</b> Used
+                            </h2>
+                            <div className='grid w-full rounded-full bg-orange px-8'>
+                                <h1 className='text-center text-3xl font-bold text-white'>
+                                    $2793
+                                </h1>
+                            </div>
+                        </div>
+                        {/* old Price tag */}
+                        {/* <div className='grid items-center justify-center pt-2'>
+                            <div className='grid w-full rounded-full bg-orange px-8'>
+                                <h1 className='text-center text-3xl font-bold text-white'>
+                                    $2793
+                                </h1>
+                            </div>
+                        </div> */}
+                    </div>
+                    {/* <div className='row-span-2 grid'> */}
+                    <div>
+                        {/* Description */}
+                        <div className='grid-row-2 grid content-start justify-start gap-3'>
+                            <div className='border-b-2 border-purple'>
+                                <h2 className='text-2xl text-darkPurple'>
+                                    Details:
+                                </h2>
+                            </div>
+                            <div>
+                                <p className='pb-2 text-extraDarkPurple'>
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Porro, quo, atque eligendi
+                                    et illum id, non maiores perspiciatis
+                                    expedita quaerat assumenda iure cupiditate
+                                    architecto modi numquam doloremque
+                                    repellendus exercitationem distinctio.
+                                    Lorem: 5 letters, ipsum: 5 letters.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    {/* <div className='grid scale-[80%] grid-cols-3  items-center justify-center gap-6 rounded-l-full bg-darkPurple'> */}
+                    <div className='grid scale-[80%] grid-cols-3 items-center justify-center gap-6 rounded-l-full bg-darkPurple'>
+                        {/* User */}
+                        <div>
+                            {/* User photo */}
+                            <img
+                                src='https://149366112.v2.pressablecdn.com/wp-content/uploads/2014/07/m1lead.jpg'
+                                alt='Cat user'
+                                className='m-3 rounded-full'
+                            />
+                        </div>
+                        <div className='col-span-2 grid text-xl text-white'>
+                            {/* User info */}
+                            <h2>Mr. nice cat</h2>
+                            <h2>Email: mrNiceCat@gmail.com</h2>
+                            <h2>Location: Istanbul/Turkeky</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='grid justify-center'>Google Map</div>
+        </div>
+    );
+};
+
+export default Details;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,51 @@
 const { fontFamily } = require("tailwindcss/defaultTheme");
+const plugin = require("tailwindcss/plugin");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
+        screens: {
+            xs: "200px",
+            sm: "640px",
+            md: "768px",
+            lg: "1024px",
+            xl: "1280px",
+            "2xl": "1536px",
+        },
         extend: {
             fontFamily: {
                 primary: ["Inter", ...fontFamily.sans],
             },
+
+            // The project custom colors
+            colors: {
+                white: "#FFFFFF",
+                clay: "#F1F6FA",
+                cyan: "#90EEE1",
+                orange: "#FF8A57",
+                purple: "#7874F2",
+                darkPurple: "#585785",
+                extraDarkPurple: "#32314D",
+                black: "#000000",
+                yellow: "#ebda41",
+            },
         },
     },
-    plugins: [require("@tailwindcss/forms")],
+    plugins: [
+        require("@tailwindcss/forms"),
+        plugin(function ({ addUtilities }) {
+            addUtilities({
+                ".scrollbar-hide": {
+                    /* Firefox */
+                    "scrollbar-width": "none",
+
+                    /* Safari and Chrome */
+                    "&::-webkit-scrollbar": {
+                        display: "none",
+                    },
+                },
+            });
+        }),
+    ],
 };
