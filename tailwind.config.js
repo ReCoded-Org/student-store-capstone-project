bc4275dc640d54a0bdc565004a9d824914be0353
Selin Su Varol <selinsuvarol@gmail.com>
2022-07-27 13:47:16 +0300

    style(tailwind.config.js): add XS screen size, correct purple hex codes
    Added a custom 350px screen size due to problems with tailwind's built in screen sizes.Two of the
    purples were the same hex code, corrected that.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
 const { fontFamily } = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    content: ["./src/*/.{js,jsx,ts,tsx}"],
     theme: {
+        screens: {
+            'xs': '350px',
+            ...defaultTheme.screens,
+          },
         extend: {
             fontFamily: {
                 Poppins: ['"Poppins"', "serif"],
             },
             colors: {
-                purple: "#7874F2",
+                purple: "#585785",
                 lightpurple: "#7874F2",
                 darkpurple: "#32314D",
                 pumpkin: "#FF8A57",
