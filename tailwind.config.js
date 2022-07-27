fd92a4f62832439f754be26e2221d8e311eca7c8
Selin Su Varol <selinsuvarol@gmail.com>
2022-07-27 14:14:53 +0300

    fix(tailwind.config.js): fix bug regarding tailwind, it was breaking gizem's navbar somehow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 const { fontFamily } = require("tailwindcss/defaultTheme");
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/*/.{js,jsx,ts,tsx}"],
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         screens: {
             'xs': '350px',
