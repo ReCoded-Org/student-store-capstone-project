83ac368cd90fee0b0e5fa84bcc93f71feb11ed39
Gizem Haspolat <gizemhaspolat@gmail.com>
2022-07-29 22:47:40 +0300

    style(buttons): color change based on tailwind config file
    #15

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -6,9 +6,9 @@ export default function Button({ buttonStyle, type, text, handleClick }) {
     const signUpBtnSignInPage =
         "h-9 w-36 m-1 p-1 rounded-lg border-2 bg-pumpkin font-extralight text-white text-sm shadow-sm";
     const signInBtnSignUpPage =
-        "h-9 w-36 m-1 p-1 rounded-lg border-2 bg-purple font-extralight text-sm text-white shadow-sm";
+        "h-9 w-36 m-1 p-1 rounded-lg border-2 bg-darkPurple font-extralight text-sm text-white shadow-sm";
     const signUpBtnSignUpPage =
-        "h-10 w-40 m-1 p-1 rounded-lg border-2 bg-purple font-extralight text-[15px] text-white shadow-sm";
+        "h-10 w-40 m-1 p-1 rounded-lg border-2 bg-darkPurple font-extralight text-[15px] text-white shadow-sm";
     const listItemBtn =
         "h-12 w-72 md:h-14 md:w-80 lg:h-14 lg:w-96 m-1 p-1 rounded-lg border-2 bg-pumpkin font-md text-[17px] text-white shadow-sm";
     const saveChangesBtn =
