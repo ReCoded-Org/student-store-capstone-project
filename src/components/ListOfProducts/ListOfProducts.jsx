5b7f539328cfaf449434ad6d0426f0187e042801
NouhRastanawi <noahrastdark@gmail.com>
2022-08-01 02:19:48 +0300

    style(singleproduct.jsx, listofproducts.jsx): change styling for more responsiveness
    All the Homepage components now are fully responsive now.
    
    #21

--- src/components/ListOfProducts/ListOfProducts.jsx
+++ src/components/ListOfProducts/ListOfProducts.jsx
@@ -4,7 +4,8 @@ import PRODUCTS from "./data";
 import SingleProduct from "./SingleProduct";
 export default function ListOfProducts() {
     return (
-        <div className='container mx-auto flex flex-wrap justify-center gap-x-6 lg:justify-start'>
+        <div className='container mx-auto flex flex-wrap justify-center gap-x-4'>
+            {/* <div className='mx-auto grid grid-flow-col justify-center gap-x-6'> */}
             {PRODUCTS.map((product) => {
                 return <SingleProduct product={product} key={product.id} />;
             })}
