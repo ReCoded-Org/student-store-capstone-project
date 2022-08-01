81c3ded7e9772e989cade4762e165b32fba61781
NouhRastanawi <noahrastdark@gmail.com>
2022-08-01 03:29:52 +0300

    style(categorypricefilter.jsx): style brush effect for headers
    Changed it from the yellow one to the cyan.
    
    #21

--- src/components/categoryPriceFilter/CategoryPriceFilter.jsx
+++ src/components/categoryPriceFilter/CategoryPriceFilter.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-import styles from "../../styles/headers.module.css";
-
 import Filter from "../filter";
 import categories from "../filter/categories";
 import PriceFilter from "../price-filter";
@@ -9,12 +7,12 @@ import PriceFilter from "../price-filter";
 const CategoryPriceFilter = () => {
     return (
         <div className='px-[15px] py-5'>
-            <div className='invisible mx-5 my-2 grid gap-5 text-xl font-bold text-purple lg:visible lg:grid-cols-3'>
+            <div className='invisible mx-5 my-2 grid gap-5 text-2xl font-bold text-purple lg:visible lg:grid-cols-3'>
                 <div className='col-span-2'>
-                    <h1 className={styles.brush}>Categories</h1>
+                    <h1 className='brush w-fit'>Categories</h1>
                 </div>
                 <div>
-                    <h1 className={styles.brush}>Price</h1>
+                    <h1 className='brush w-fit'>Price</h1>
                 </div>
             </div>
             <div className='grid overflow-hidden rounded-full bg-white xxs:grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3'>
