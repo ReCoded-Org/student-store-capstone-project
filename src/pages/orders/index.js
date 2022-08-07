f0741cf7bcd022564698ed6f15a7c51294227542
Selin Su Varol <selinsuvarol@gmail.com>
2022-08-07 22:56:45 +0300

    fix(sidebar.jsx): add props to each sidebar
    Updated 2 test files(editProfile + Sidebar) as well
    
    fix #42

--- src/pages/orders/index.js
+++ src/pages/orders/index.js
@@ -8,7 +8,7 @@ function OrdersPage() {
     return (
         <Layout>
             <main className='flex xs:flex-col sm:flex-col md:flex-row lg:flex-row'>
-                <Sidebar />
+                <Sidebar pageName="orders" />
                 <Orders />
             </main>
         </Layout>
