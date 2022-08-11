a66b414e7c569f11bcfd6228c52617ec2607ffa5
NouhRastanawi <noahrastdark@gmail.com>
2022-08-11 04:42:14 +0300

    feat(sign-up.jsx, sign-in.jsx): add German translation

--- src/pages/sign-up.jsx
+++ src/pages/sign-up.jsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import React from "react";
 import { BsFacebook, BsGoogle, BsTwitter } from "react-icons/bs";
 
@@ -7,6 +9,7 @@ import Input from "@/components/input";
 import Layout from "@/components/layout/Layout";
 
 export default function Signup() {
+    const { t } = useTranslation("sign");
     return (
         <Layout>
             <div className='signup-container flex   justify-center  md:flex-row'>
@@ -15,35 +18,38 @@ export default function Signup() {
                 <div className='inputs  container  w-3/6 items-center'>
                     <div className='container m-auto flex w-5/6 flex-col items-center'>
                         <h1 className='my-2 py-6 font-Ubuntu text-4xl text-white md:my-3 md:text-5xl'>
-                            Sign-Up
+                            {t("sign-up")}
                         </h1>
                         <form
                             action='submit'
                             className='container m-auto flex w-5/6 flex-col items-center'
                         >
-                            <Input placeholder='Name' type='text' />
-                            <Input placeholder='Surname' type='text' />
-                            <Input placeholder='e-mail address' type='email' />
-                            <Input placeholder='School name' type='text' />
-                            <Input placeholder='Password' type='Password' />
+                            <Input placeholder={t("name")} type='text' />
+                            <Input placeholder={t("surname")} type='text' />
+                            <Input placeholder='e-mail' type='email' />
+                            <Input placeholder={t("university")} type='text' />
                             <Input
-                                placeholder='re-enter Password'
+                                placeholder={t("password")}
+                                type='Password'
+                            />
+                            <Input
+                                placeholder={t("confirm-password")}
                                 type='Password'
                             />
 
                             <Button
                                 buttonStyle='purpleSignUp'
                                 type='submit'
-                                text='Sign-up'
+                                text={t("sign-up")}
                             />
                         </form>
                         <div className='flex items-center'>
                             <span className='line'></span>
-                            <div>or</div>
+                            <div>{t("or")}</div>
                             <span className='line'></span>
                         </div>
                         <div className='m-1 text-lg text-iceblue'>
-                            Sign up with
+                            {t("Sign-up-with")}
                         </div>
                         <div className='m-1 mb-8 flex flex-row  '>
                             <button className=' m-1 flex items-center rounded-3xl border border-[#F26F6F] p-1  text-[#F26F6F]'>
@@ -72,7 +78,7 @@ export default function Signup() {
                             </button>
                         </div>
                         <div className='mb-4 text-xl text-iceblue'>
-                            Already Have an account?
+                            {t("already-have-an-account")}?
                         </div>
                         <div className='mb-8'>
                             <Link href='/sign-in'>
@@ -80,7 +86,7 @@ export default function Signup() {
                                     <Button
                                         buttonStyle='purpleSignUp'
                                         type='submit'
-                                        text='Sign-in'
+                                        text={t("sign-in")}
                                     />
                                 </a>
                             </Link>
@@ -90,4 +96,18 @@ export default function Signup() {
             </div>
         </Layout>
     );
+}
+
+export async function getStaticProps({ locale }) {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, [
+                "common",
+                "header",
+                "footer",
+                "sign",
+            ])),
+            // Will be passed to the page component as props
+        },
+    };
 }
