a3f4b3e2f219ca9978ee0e341c6d3ac4d2096d2c
Gizem Haspolat <gizemhaspolat@gmail.com>
2022-08-25 03:40:33 +0300

    feat(header): add userIcon dropdown menu
    make the dropdowns disappear whereever is clicked on the page itself - add translations to the
    dropdown list - edit donation translation
    
    #45

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useTranslation } from "next-i18next";
 import * as React from "react";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { BiSearchAlt } from "react-icons/bi";
 import { IoIosArrowDown } from "react-icons/io";
 import { RiUser3Line } from "react-icons/ri";
@@ -25,7 +25,26 @@ function Header({
     const { t } = useTranslation("header");
     const [isNavOpen, setIsNavOpen] = useState(false);
     const [isLanguageOpen, setIsLanguageOpen] = useState(false);
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
+    const [userIconClicked, setUserIconClicked] = useState(false);
+
+    const languageRef = useRef(null);
+    const userIconRef = useRef(null);
+
+    const handleClickOutside = (e) => {
+        if (languageRef.current && !languageRef.current.contains(e.target)) {
+            setIsLanguageOpen(false);
+        }
+        if (userIconRef.current && !userIconRef.current.contains(e.target)) {
+            setUserIconClicked(false);
+        }
+    };
+    useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
 
     function searchByName() {
         setfilteredProducts(
@@ -165,7 +184,7 @@ function Header({
                             <div className='mr-2 rounded-[20px] border-[3px] border-white hover:border-pumpkin'>
                                 <button
                                     type='button'
-                                    className='flex w-full items-center justify-center  rounded-full bg-white p-1 text-sm font-medium text-pumpkin hover:border-pumpkin'
+                                    className='flex w-full items-center justify-center  rounded-full bg-white p-1 text-sm font-medium text-pumpkin blur-none hover:border-pumpkin'
                                     id='menu-button'
                                     aria-expanded='true'
                                     aria-haspopup='true'
@@ -180,7 +199,7 @@ function Header({
 
                             <div
                                 className={`
-                                    absolute right-0 z-10 mt-2 flex rounded-md  
+                                    absolute right-2 z-10 mt-2 flex rounded-md  
                                                                        bg-white
                                                                            shadow-md ring-1
                                                                            ring-black ring-opacity-5 focus:outline-none
@@ -189,6 +208,7 @@ function Header({
                                                                                     ? "showLanguage"
                                                                                     : "hideLanguage"
                                                                             }`}
+                                ref={languageRef}
                                 role='menu'
                                 aria-orientation='vertical'
                                 aria-labelledby='menu-button'
@@ -200,7 +220,7 @@ function Header({
                                 >
                                     <Link href='/' locale='en'>
                                         <a
-                                            className='flashing block px-4 py-2 text-sm text-gray-700'
+                                            className='flashing block px-4 py-2 text-sm text-pumpkin'
                                             role='menuitem'
                                             tabIndex='-1'
                                             id='menu-item-0'
@@ -211,7 +231,7 @@ function Header({
 
                                     <Link href='/' locale='tr'>
                                         <a
-                                            className='flashing block px-4 py-2 text-sm text-gray-700'
+                                            className='flashing block px-4 py-2 text-sm text-pumpkin'
                                             role='menuitem'
                                             tabIndex='-1'
                                             id='menu-item-1'
@@ -222,7 +242,7 @@ function Header({
 
                                     <Link href='/' locale='de'>
                                         <a
-                                            className='flashing block px-4 py-2 text-sm text-gray-700'
+                                            className='flashing block px-4 py-2 text-sm text-pumpkin'
                                             role='menuitem'
                                             tabIndex='-1'
                                             id='menu-item-1'
@@ -234,17 +254,28 @@ function Header({
                             </div>
                         </div>
 
-                        <div className='text-md hidden md:flex md:w-auto md:items-center '>
+                        <div className=' hidden w-auto items-center md:flex md:flex-row '>
                             {isLoggedIn ? (
-                                <div className=' mr-2 flex w-auto items-center self-center overflow-hidden rounded-full border-[3px] border-white p-1 hover:border-purple  md:flex lg:flex'>
-                                    {/* write a function that checks if there is a user img, if not, falls back to this icon  */}
+                                <div className='relative inline-block text-left'>
+                                    <div className='mr-2'>
+                                        <button
+                                            type='button'
+                                            className='mr-2 flex w-auto items-center self-center overflow-hidden rounded-full border-[3px] border-white p-1 hover:border-purple  md:flex lg:flex'
+                                            id='menu-button'
+                                            aria-expanded='true'
+                                            aria-haspopup='true'
+                                            onClick={() =>
+                                                setUserIconClicked(
+                                                    (prev) => !prev
+                                                )
+                                            }
+                                        >
+                                            <RiUser3Line
+                                                size={24}
+                                                className='text-lightpurple'
+                                            />
 
-                                    <RiUser3Line
-                                        size={24}
-                                        className='text-lightpurple'
-                                    />
-
-                                    {/* <Image
+                                            {/* <Image
                                         width={32}
                                         height={32}
                                         // src={userIcon}
@@ -253,10 +284,73 @@ function Header({
                                         layout='intrinsic'
                                         className='rounded-full'
                                     /> */}
-                                    <IoIosArrowDown
-                                        size={18}
-                                        className='m-1 text-lightpurple'
-                                    />
+                                            <IoIosArrowDown
+                                                size={18}
+                                                className='m-1 text-lightpurple'
+                                            />
+                                        </button>
+                                    </div>
+
+                                    <div
+                                        className={`
+                                    absolute left-1 z-10 mt-2 flex rounded-md  
+                                                                       bg-white
+                                                                           shadow-md ring-2
+                                                                           ring-lightpurple ring-opacity-5
+                                                                            ${
+                                                                                userIconClicked
+                                                                                    ? "showUserMenu"
+                                                                                    : "hideUserMenu"
+                                                                            }`}
+                                        ref={userIconRef}
+                                        role='menu'
+                                        aria-orientation='vertical'
+                                        aria-labelledby='menu-button'
+                                        tabIndex='-1'
+                                    >
+                                        <div className='right-0 flex flex-col items-center py-1 '>
+                                            <Link href='/listings'>
+                                                <a
+                                                    className='flashing block px-4 py-2 text-sm text-lightpurple'
+                                                    role='menuitem'
+                                                    tabIndex='-1'
+                                                    id='menu-item-0'
+                                                >
+                                                    {t("my-listings")}
+                                                </a>
+                                            </Link>
+
+                                            <Link href='/edit-profile'>
+                                                <a
+                                                    className='flashing block px-4 py-2 text-sm text-lightpurple'
+                                                    role='menuitem'
+                                                    tabIndex='-1'
+                                                    id='menu-item-1'
+                                                >
+                                                    {t("edit-Profile")}
+                                                </a>
+                                            </Link>
+
+                                            <Link href='/donation'>
+                                                <a
+                                                    className='flashing block px-4 py-2 text-sm text-lightpurple'
+                                                    role='menuitem'
+                                                    tabIndex='-1'
+                                                    id='menu-item-1'
+                                                >
+                                                    {t("donate")}
+                                                </a>
+                                            </Link>
+                                            <a
+                                                className='flashing block px-4 py-2 text-sm text-lightpurple'
+                                                role='menuitem'
+                                                tabIndex='-1'
+                                                id='menu-item-1'
+                                            >
+                                                {t("logout")}
+                                            </a>
+                                        </div>
+                                    </div>
                                 </div>
                             ) : (
                                 <Link href='/sign-in'>
@@ -268,13 +362,13 @@ function Header({
                                     </a>
                                 </Link>
                             )}
-
-                            <Link href='/product-listing'>
-                                <a className=' text-md mr-2 block rounded-[20px]  bg-darkPurple py-2 px-4 text-white hover:bg-purple'>
-                                    {t("sell-items")}
-                                </a>
-                            </Link>
                         </div>
+
+                        <Link href='/product-listing'>
+                            <a className=' text-md mr-2 block rounded-[20px]  bg-darkPurple py-2 px-4 text-white hover:bg-purple'>
+                                {t("sell-items")}
+                            </a>
+                        </Link>
                     </div>
                 </div>
                 <style>{`
@@ -297,8 +391,20 @@ function Header({
       }
       .showLanguage {
         display: block;
+        width: 100%;
+        height: auto;
+        
         }
         .hideLanguage {
+            display: none;
+        }
+        .showUserMenu {
+            display: block;
+            width: 20vh;
+            height: auto;
+            text-align: start;
+        }
+        .hideUserMenu {
             display: none;
         }
       `}</style>
