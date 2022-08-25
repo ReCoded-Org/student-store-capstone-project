802667626ffc6f054287c3bcd1569401101854f4
Mohamed Hadri <mhadri1000@gmail.com>
2022-08-25 21:19:19 +0300

    feat: added favicon and title
    feat #83

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,9 +1,18 @@
+import Head from "next/head";
 import { appWithTranslation } from "next-i18next";
 
 import "@/styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>Students Store</title>
+                <link rel='icon' href='/favicon.png' />
+            </Head>
+            <Component {...pageProps} />;
+        </>
+    );
 }
 
 export default appWithTranslation(MyApp);
