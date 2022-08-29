4e2a4e5bf996b62787056d02c024f1f53063ec0c
Mohamed Hadri <mhadri1000@gmail.com>
2022-08-29 12:31:38 +0300

    fix: deleted protected routes folder
    fix 83

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 // api example
 export default function hello(req, res) {
     res.status(200).json({ name: "api is working" });
