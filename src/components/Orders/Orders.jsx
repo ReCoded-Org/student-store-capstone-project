21ead2203400f227260d4aaba8f2ebcfd28a134c
Salah Al-Dhaferi <binaryminded@gmail.com>
2022-08-31 14:42:33 +0300

    build: fixed some path issues

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,7 +1,124 @@
 import React from "react";
 
-import Order from "./order";
-import orderList from "./orderlist";
+import Order from "./Order";
+
+const orderList = [
+    {
+        id: 1,
+        img: "https://productimages.hepsiburada.net/s/32/375/10354042765362.jpg",
+        title: "Apple iPad Pro ",
+        price: "4000",
+        category: "ELECTRONICS",
+        location: "Istanbul",
+        description:
+            "Apple iPad Pro - 12.9-inch - Wi-Fi - 64GB - Space Gray, minor scratches",
+        orderDate: "2022-04-01",
+        deliveryAddress: "Besiktas Mh. Besiktas Cad. No:99/9 Besiktas/Istanbul",
+    },
+    {
+        id: 2,
+        img: "https://pyxis.nymag.com/v1/imgs/86c/882/b47a32d8e72df7aeadf74490a82aaa9432.rsquare.w600.jpg",
+        title: "Leather Couch",
+        price: "1000",
+        category: "FURNITURE",
+        location: "Izmir",
+        description: "Leather Couch - Brown, doesn't have stains",
+        orderDate: "2022-07-01",
+        deliveryAddress: "Besiktas Mh. Besiktas Cad. No:99/9 Besiktas/Istanbul",
+    },
+    {
+        id: 3,
+        img: "https://productimages.hepsiburada.net/s/32/375/10354042765362.jpg",
+        title: "Apple iPad Pro ",
+        price: "4000",
+        category: "ELECTRONICS",
+        location: "Istanbul",
+        description:
+            "Apple iPad Pro - 12.9-inch - Wi-Fi - 64GB - Space Gray, minor scratches",
+        orderDate: "2022-04-01",
+        deliveryAddress: "Besiktas Mh. Besiktas Cad. No:99/9 Besiktas/Istanbul",
+    },
+    {
+        id: 4,
+        img: "https://pyxis.nymag.com/v1/imgs/86c/882/b47a32d8e72df7aeadf74490a82aaa9432.rsquare.w600.jpg",
+        title: "Leather Couch",
+        price: "1000",
+        category: "FURNITURE",
+        location: "Izmir",
+        description: "Leather Couch - Brown, doesn't have stains",
+        orderDate: "2022-07-01",
+        deliveryAddress: "Besiktas Mh. Besiktas Cad. No:99/9 Besiktas/Istanbul",
+    },
+    {
+        id: 5,
+        img: "https://productimages.hepsiburada.net/s/32/375/10354042765362.jpg",
+        title: "Apple iPad Pro ",
+        price: "4000",
+        category: "ELECTRONICS",
+        location: "Istanbul",
+        description:
+            "Apple iPad Pro - 12.9-inch - Wi-Fi - 64GB - Space Gray, minor scratches",
+        orderDate: "2022-04-01",
+        deliveryAddress: "Besiktas Mh. Besiktas Cad. No:99/9 Besiktas/Istanbul",
+    },
+    {
+        id: 6,
+        img: "https://pyxis.nymag.com/v1/imgs/86c/882/b47a32d8e72df7aeadf74490a82aaa9432.rsquare.w600.jpg",
+        title: "Leather Couch",
+        price: "1000",
+        category: "FURNITURE",
+        location: "Izmir",
+        description: "Leather Couch - Brown, doesn't have stains",
+        orderDate: "2022-07-01",
+        deliveryAddress: "Besiktas Mh. Besiktas Cad. No:99/9 Besiktas/Istanbul",
+    },
+    {
+        id: 7,
+        img: "https://productimages.hepsiburada.net/s/32/375/10354042765362.jpg",
+        title: "Apple iPad Pro ",
+        price: "4000",
+        category: "ELECTRONICS",
+        location: "Istanbul",
+        description:
+            "Apple iPad Pro - 12.9-inch - Wi-Fi - 64GB - Space Gray, minor scratches",
+        orderDate: "2022-04-01",
+        deliveryAddress: "Besiktas Mh. Besiktas Cad. No:99/9 Besiktas/Istanbul",
+    },
+    {
+        id: 8,
+        img: "https://pyxis.nymag.com/v1/imgs/86c/882/b47a32d8e72df7aeadf74490a82aaa9432.rsquare.w600.jpg",
+        title: "Leather Couch",
+        price: "1000",
+        category: "FURNITURE",
+        location: "Izmir",
+        description: "Leather Couch - Brown, doesn't have stains",
+        orderDate: "2022-07-01",
+        deliveryAddress: "Besiktas Mh. Besiktas Cad. No:99/9 Besiktas/Istanbul",
+    },
+    {
+        id: 9,
+        img: "https://productimages.hepsiburada.net/s/32/375/10354042765362.jpg",
+        title: "Apple iPad Pro ",
+        price: "4000",
+        category: "ELECTRONICS",
+        location: "Istanbul",
+        description:
+            "Apple iPad Pro - 12.9-inch - Wi-Fi - 64GB - Space Gray, minor scratches",
+        orderDate: "2022-04-01",
+        deliveryAddress: "Besiktas Mh. Besiktas Cad. No:99/9 Besiktas/Istanbul",
+    },
+    {
+        id: 10,
+        img: "https://pyxis.nymag.com/v1/imgs/86c/882/b47a32d8e72df7aeadf74490a82aaa9432.rsquare.w600.jpg",
+        title: "Leather Couch",
+        price: "1000",
+        category: "FURNITURE",
+        location: "Izmir",
+        description: "Leather Couch - Brown, doesn't have stains",
+        orderDate: "2022-07-01",
+        deliveryAddress: "Besiktas Mh. Besiktas Cad. No:99/9 Besiktas/Istanbul",
+    },
+];
 
 export default function Orders() {
     return (
